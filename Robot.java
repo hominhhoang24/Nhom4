dbf1ea82be779072a92b74b4ba278b114e4a957f
hominhhoang24 <hominhhung2724@gmail.com>
2024-07-24 10:38:04 +0700

    add file

---
+++ Robot.java
@@ -0,0 +1,23 @@
+public class Robot {
+        private String name;
+
+        /**
+         * Xay dung Robot.
+         *
+         * @param name ten Robot
+         */
+        public Robot(String name) {
+            this.name = name;
+        }
+
+        /**
+         * Tra ve Robot.
+         *
+         * @return ten Robot
+         */
+        public String getName() {
+            return name;
+        }
+    }
+
+}
